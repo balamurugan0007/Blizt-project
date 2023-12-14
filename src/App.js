b4e162a59e213eb0be06299e4c4570ba965e0bfa
balamurugan <balamurugan.k.02437@gmail.com>
2023-12-14 13:24:40 +0530

    ds

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 
 
 
-import { BrowserRouter as Router ,Routes,Route } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Update } from "./pages/Update";
+
 function App() {
   return (
-    <Router>
-      <Navbar/>
-        <Routes>
-            <Route path="" element={<Home/>}/>
-            <Route path="/profile/:id" element={<Profile/>}/>
-            <Route path="/profile/update" element={<Update/>}/>
-        </Routes>
-    </Router>
+    <div></div>
               
   );
 }
